67d2939a2c4d6bbbf43cea55054507b8815c8249
YanLucass <yanlucascarvalho18@gmail.com>
2024-01-07 16:02:21 -0300

    add errors middleware

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,10 @@
+import { AppError } from "@shared/errors/AppError";
 import { Router } from "express";
 
 const router = Router();
 
 router.get("/", (req, res) => {
-  return res.json({ message: "Hello World" });
+  throw new AppError("Acesso negado", 401);
 });
 
 export { router };
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,8 +1,11 @@
 import "dotenv/config";
-import express from "express";
+import express, { NextFunction, Request, Response, response } from "express";
 import "express-async-errors";
 import cors from "cors";
 import { router } from "./routes";
+
+// importar classe para tramento de erro.
+import { AppError } from "@shared/errors/AppError";
 
 const app = express();
 app.use(cors());
@@ -11,6 +14,24 @@ app.use(express.json());
 
 //define routes only file (index.ts)
 app.use(router);
+
+// caso tenha ocorrido algum erro nas rotas vamos interceptar aqui.
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  //caso o erro seja uma instancia de AppError vamos retornar um objeto com status e mesagem do erro;
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+
+  //erros da aplicação ex: erros interno do servidor (500)
+  console.log(error);
+  return res.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server stard on port ${process.env.PORT}`);
