fd8abe2ab8b1c42daaeb00ffb8a8680e1cee350b
YanLucass <yanlucascarvalho18@gmail.com>
2024-01-07 11:28:30 -0300

    config routes

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,15 +2,15 @@ import "dotenv/config";
 import express from "express";
 import "express-async-errors";
 import cors from "cors";
+import { router } from "./routes";
 
 const app = express();
 app.use(cors());
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello world" });
-});
+//define routes only file (index.ts)
+app.use(router);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server stard on port ${process.env.PORT}`);
