d5b782d5bcff7d15d5c9eefc5c162f789e9e9bc9
YanLucass <yanlucascarvalho18@gmail.com>
2024-01-17 17:31:47 -0300

    create ShowRoleController / ShowRolleUseCase and router

--- src/roles/http/routes/rolesRouter.ts
+++ src/roles/http/routes/rolesRouter.ts
@@ -5,6 +5,7 @@ const roulesRouter = Router();
 //importar instancia do controller
 import { createRoleController } from "@roles/usesCases/createRole/instancias";
 import { listRolesController } from "@roles/usesCases/listRoles/instancias";
+import { showRoleController } from "@roles/usesCases/showRole/instancias";
 //simular bd
 
 // criar roles
@@ -13,9 +14,14 @@ roulesRouter.post("/", (req, res) => {
 });
 
 //pegar roles
-
 roulesRouter.get("/", (req, res) => {
    return listRolesController.handle(req, res);
+});
+
+//pegar role por id
+
+roulesRouter.get("/:id", (req, res) => {
+   return showRoleController.handle(req, res);
 });
 
 export { roulesRouter };
