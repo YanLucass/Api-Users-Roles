a67cff6c133445487fb992a1e751e485f243d223
YanLucass <yanlucascarvalho18@gmail.com>
2024-02-04 13:38:15 -0300

    create migration AddRoleIdToUsersTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -2,6 +2,7 @@ import { DataSource } from "typeorm";
 //migrations
 import { CreateRolesTable1705428558994 } from "./migrations/1705428558994-CreateRolesTable";
 import { CreateUsersTable1707062283935 } from "./migrations/1707062283935-CreateUsersTable";
+import { AddRoleIdToUsersTable1707063703772 } from "./migrations/1707063703772-AddRoleIdToUsersTable";
 import { Role } from "@roles/entities/Role";
 
 //postgres
@@ -13,5 +14,9 @@ export const PostgresDataSource = new DataSource({
    password: "180",
    database: "myapii",
    entities: [Role],
-   migrations: [CreateRolesTable1705428558994, CreateUsersTable1707062283935],
+   migrations: [
+      CreateRolesTable1705428558994,
+      CreateUsersTable1707062283935,
+      AddRoleIdToUsersTable1707063703772,
+   ],
 });
