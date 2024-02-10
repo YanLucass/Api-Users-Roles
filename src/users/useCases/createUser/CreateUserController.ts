73115b2f8b376b8c1b31cac1feaefe490fdf7107
YanLucass <yanlucascarvalho18@gmail.com>
2024-02-10 16:43:26 -0300

    create user controller

---
+++ src/users/useCases/createUser/CreateUserController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+export class CreateUserController {
+   async handle(req: Request, res: Response): Promise<Response> {
+      //get createUserUseCase instance
+      const createUserUseCase = container.resolve(CreateUserUseCase);
+      const { name, email, password, isAdmin, roleId } = req.body;
+      //create user
+      const user = await createUserUseCase.execute({ name, email, password, isAdmin, roleId });
+      return res.status(201).json({ message: "new user created", user });
+   }
+}
