0a170d0c9c8bc30106193574e8f85f2ea271b0b4
YanLucass <yanlucascarvalho18@gmail.com>
2024-02-11 16:14:42 -0300

    Created CreateLoginUseCase, CreateLoginController

--- src/users/container/index.ts
+++ src/users/container/index.ts
@@ -3,7 +3,9 @@ import { IUsersRepository } from "@users/repositories/IUsersRepository";
 import { UsersRepository } from "@users/repositories/UsersRepository";
 import { CreateUserController } from "@users/useCases/createUser/CreateUserController";
 import { ListUsersController } from "@users/useCases/listUsers/ListUsersController";
+import { CreateLoginController } from "@users/useCases/createLogin/CreateLoginController";
 container.registerSingleton<IUsersRepository>("UsersRepository", UsersRepository);
 //controller
 container.registerSingleton("CreateUserController", CreateUserController);
 container.registerSingleton("ListUserController", ListUsersController);
+container.registerSingleton("CreateLoginController", CreateLoginController);
